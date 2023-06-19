74f4767c1683f0f6626adb9cfdf97951653b5909
Balaji Srinivasan <93860462+pyCoder03@users.noreply.github.com>
2023-06-19 23:13:02 +0530

    Update Bus_Interface_Unit.v

--- Bus_Interface_Unit.v
+++ Bus_Interface_Unit.v
@@ -22,9 +22,20 @@
 
 module Bus_Interface_Unit(ale,den_,rd_,wr_,dtr_syn,dtr_,busint,t_state);
 input dtr_,busint;
-output reg rd_=1'b1,wr_=1'b1,ale=1'b0,den_=1'b1,dtr_syn;
 input[2:0] t_state=3'b000;
-reg sig=1,busint_syn=0;
+output rd_,wr_,ale,den_;
+output reg dtr_syn;
+
+reg rd_=1'b1,wr_=1'b1;
+reg busint_syn=0;
+wire sig,den_,a,b,ta,ale;
+
+wire t2,t1,t0;
+assign {t2,t1,t0}=t_state;
+assign ta=t2&t1&t0;
+//assign rd_=rd_,wr_=wr_,ale=ale_,den_=den_,sig=sig;
+
+//assign rd_=(
 always @(*) begin
     if(busint_syn==0) begin
         rd_=1'b1;
@@ -39,28 +50,12 @@ always @(*) begin
         wr_=1'b1;
         end
 end
-always @(*) begin
-    case(t_state)
-        3'b000: begin
-            dtr_syn=dtr_;
-            busint_syn=busint;
-            if(busint_syn) ale=1'b1;
-            end
-        3'b010: begin
-            if(busint_syn) begin
-                ale=1'b0;
-                sig=1'b0;
-                den_=1'b0;
-                end
-            end
-        3'b110: begin
-            if(busint_syn) begin    
-                sig=1'b1;
-                end
-            end
-        3'b111: begin
-            if(busint_syn) den_=1'b1;
-            end
-    endcase
+
+always @(negedge ta) begin
+        dtr_syn=dtr_;
+        busint_syn=busint;
 end
+
+assign a=t2&t1,b=~(t2|t1),sig=(~busint_syn)|a|b,den_=(~busint_syn)|(a&t0)|b,ale=busint_syn&~(t2|t1);
+
 endmodule
